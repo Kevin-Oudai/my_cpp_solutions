a3eabb4a5295c946a3ca47813504c9fe50f13276
Kevin Oudai <46614071+Kevin-Oudai@users.noreply.github.com>
2025-08-07 15:00:59 -0400

    Implement generic selection sort and test arrays

--- chapter_12/exercise12_02.cpp
+++ chapter_12/exercise12_02.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
 #include <string>
 
+// Generic selection sort that sorts an array of type T in ascending order
 template <typename T>
-void selectionSort(T list[], int listSize)
+void selectionSort(T* list, int listSize)
 {
     for (int i = 0; i < listSize - 1; i++)
     {
-        T currentMin = list[i];
         int currentMinIndex = i;
-
         for (int j = i + 1; j < listSize; j++)
         {
-            if (currentMin > list[j])
+            if (list[j] < list[currentMinIndex])
             {
-                currentMin = list[j];
                 currentMinIndex = j;
             }
         }
 
         if (currentMinIndex != i)
         {
-            list[currentMinIndex] = list[i];
-            list[i] = currentMin;
+            T temp = list[i];
+            list[i] = list[currentMinIndex];
+            list[currentMinIndex] = temp;
         }
     }
+}
+
+// Helper function to print an array
+template <typename T>
+void printArray(const T* list, int listSize)
+{
     for (int i = 0; i < listSize; i++)
     {
-        std::cout << list[i] << " ";
+        std::cout << list[i];
+        if (i < listSize - 1)
+        {
+            std::cout << " ";
+        }
     }
     std::cout << std::endl;
 }
@@ -36,9 +45,25 @@ int main()
     int integers[] = {2, 6, 9, 10, 30, 21};
     double decimals[] = {1.2, 3.9, 2.1, 9.3, 4.5, 3.5};
     std::string strings[] = {"Kevin", "Venita", "Athena"};
+
+    std::cout << "Before sorting integers: ";
+    printArray(integers, 6);
     selectionSort(integers, 6);
+    std::cout << "After sorting integers:  ";
+    printArray(integers, 6);
+
+    std::cout << "\nBefore sorting decimals: ";
+    printArray(decimals, 6);
     selectionSort(decimals, 6);
+    std::cout << "After sorting decimals:  ";
+    printArray(decimals, 6);
+
+    std::cout << "\nBefore sorting strings:  ";
+    printArray(strings, 3);
     selectionSort(strings, 3);
+    std::cout << "After sorting strings:   ";
+    printArray(strings, 3);
 
     return 0;
 }
+
